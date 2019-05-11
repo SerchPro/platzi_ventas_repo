06f000b262b2643adc206d5e78af12e61cc24ce7
serch <proyects_serch@hotmail.com>
2019-05-10 20:07:04 -0500

    Se añadio la funcionanlidad de borrar un cliente

--- platzi_ventas.py
+++ platzi_ventas.py
@@ -8,13 +8,27 @@ clients="Sergio,Erick"
 opcion=input("Escribe la opcion")
 
 def get_name():
-    client_name=input("Ingresa el nombre del cliente")
+    client_name=input("Ingresa el nombre del cliente -")
     return client_name
 
 if opcion.upper()=='C':
     client_name=get_name()
     clients+=','+client_name
     print(clients)
+elif opcion.upper()=='R':
+    print(clients)
+elif opcion.upper()=='U':
+    client_name=get_name()
+
+elif opcion.upper()=='D':
+    client_name=get_name()
+    if client_name in clients:
+        print("Si se encuentra")
+        print(clients)
+        new_clients=clients.replace(client_name," ")
+        print(new_clients)
+    else:
+        print("Lo lamento, no se encuentra el usuario")
 else:
     print("Lo lamento esa opcion no existe")
 
